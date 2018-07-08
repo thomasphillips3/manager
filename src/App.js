b75f814e157c45be074238e443fc13246fc91864
Thomas Phillips <thomasphillips3@gmail.com>
2018-07-08 12:43:04 -0700

    Removed unused import

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux'; 
 import ReduxThunk from 'redux-thunk';
 import firebase from 'firebase';
-import LoginForm from './components/LoginForm';
 import reducers from './reducers';
 import Router from './router';
 
