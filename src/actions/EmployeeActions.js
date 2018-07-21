4950082c160a79874dfe9b63f6cf76d5f959423a
Thomas Phillips <thomasphillips3@gmail.com>
2018-07-20 18:10:39 -0700

    Fetch employees from Firebase

--- src/actions/EmployeeActions.js
+++ src/actions/EmployeeActions.js
@@ -1,6 +1,10 @@
 import firebase from 'firebase';
 import { Actions } from 'react-native-router-flux';
-import { EMPLOYEE_UPDATE, EMPLOYEE_CREATE } from './types'; 
+import { 
+    EMPLOYEE_UPDATE, 
+    EMPLOYEE_CREATE, 
+    EMPLOYEES_FETCH_SUCCESS 
+} from './types'; 
 
 export const employeeUpdate = ({ prop, value }) => {
     return {
@@ -17,7 +21,18 @@ export const employeeCreate = ({ name, phone, shift }) => {
             .push({ name, phone, shift })
             .then(() => {
                 dispatch({ type: EMPLOYEE_CREATE });
-                Actions.main({ type: 'reset' });
+                Actions.pop();
+            });
+    };
+};
+
+export const employeesFetch = () => {
+    const { currentUser } = firebase.auth();
+
+    return (dispatch) => {
+        firebase.database().ref(`/users/${currentUser.uid}/employees`)
+            .on('value', snapshot => {
+                dispatch({ type: EMPLOYEES_FETCH_SUCCESS, payload: snapshot.val() });
             });
     };
 };
